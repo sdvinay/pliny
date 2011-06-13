51d60b74c1d31ae366e0dc62f1a3456d9534b1dc
Vinay Kumar <sdvinay@gmail.com>
2011-06-12 23:05:50 -0700

    Document fetch_data.sh's usage of cookies

--- fetch_data.sh
+++ fetch_data.sh
@@ -2,6 +2,11 @@
 
 wget 'http://aud13.sports.sp2.yahoo.com/mlb/players.txt' --output-document=data/players.txt
 
+# This assumes a file called yahoo_auth_cookies.txt, which includes the yahoo
+# auth cookie in a format that can be consumed directly as a header.  Specifically,
+#   Cookie:Y=<value>; T=<value>
+# For obvious reasons, I'm not checking in the file yahoo_auth_cookies.txt
+# Without this cookies file, you won't be able to download stattracker data
 YAHOO_AUTH_COOKIE_HEADER=$(cat yahoo_auth_cookies.txt)
 
 wget --header=${YAHOO_AUTH_COOKIE_HEADER} \
