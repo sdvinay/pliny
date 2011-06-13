63f6d2da8013a60c1d98a0d4618a708f2f9be03a
Vinay Kumar <sdvinay@gmail.com>
2011-06-12 23:07:18 -0700

    Non-substantive changes to generate_playermap.py; rename variables for readability

--- generate_playermap.py
+++ generate_playermap.py
@@ -3,7 +3,7 @@
 import sys
 
 # put the player names in a map
-player_name = dict()
+player_names = dict()
 
 for player_line in open('data/players.txt', 'r').readlines():
 	if (player_line.startswith("m|")):
@@ -11,20 +11,20 @@ for player_line in open('data/players.txt', 'r').readlines():
 		player_data = player_line.split("|")
 		pname = "%s %s" % (player_data[3], player_data[4])
 		pnum = player_data[1]
-		player_name[pnum] = pname
+		player_names[pnum] = pname
 
 f = open('data/stattracker_data.html', 'r')
-player_line = filter(lambda str: str.startswith("players^"), f.readlines())[0]
-player_list = player_line.split("^")[1:]
-for player_data_line in player_list:
+line_with_all_player_data = filter(lambda str: str.startswith("players^"), f.readlines())[0]
+player_data_list = line_with_all_player_data.split("^")[1:]
+for player_data_line in player_data_list:
 	#print player_data_line
 	player_data = player_data_line.split("|")
 	pnum = player_data[0]
-	player_name[pnum] = player_data[1]
-	#print player_name
+	player_names[pnum] = player_data[1]
+	#print player_names
 	#	print pnum, player_data[1]
 
 outfile = open('data/playermap.txt', 'w')
-for pnum in player_name:
-	outfile.write("%s,%s\n" % (pnum, player_name[pnum]))
+for pnum in player_names:
+	outfile.write("%s,%s\n" % (pnum, player_names[pnum]))
 
