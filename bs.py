002f27250955ac1dd20b57f5a4c6f203a298b564
Vinay Kumar <sdvinay@gmail.com>
2013-08-09 16:03:38 -0700

    Use BeautifulSoup to parse team totals out of html

---
+++ bs.py
@@ -0,0 +1,41 @@
+#!/opt/local/bin/python2.7
+
+from bs4 import BeautifulSoup
+import requests
+
+URL = 'http://baseball.fantasysports.yahoo.com/b1/58506/team'
+cookie_value = open('cook2.txt').read()
+headers = {'Cookie' : cookie_value}
+
+NUMTEAMS = 20
+DATE = '2013-08-03'
+
+def convert_to_num(stri):
+	if(stri == '-'):
+		return 0
+	return float(stri)
+
+for team in range(1,NUMTEAMS+1):
+#for team in range(5,6):
+
+	payload = {'date': DATE, 'mid': team, 'week': 1}
+	r = requests.get(URL, params=payload, headers=headers)
+	soup = BeautifulSoup(r.text)
+
+	batting_team_totals = soup.find_all('div', 'ptstotal')[0].find_all('li')
+	AB = int(convert_to_num(batting_team_totals[1].text))
+	H  = int(convert_to_num(batting_team_totals[2].text))
+	DB = int(convert_to_num(batting_team_totals[3].text))
+	TP = int(convert_to_num(batting_team_totals[4].text))
+	HR = int(convert_to_num(batting_team_totals[5].text))
+	SB = int(convert_to_num(batting_team_totals[6].text))
+	CS = int(convert_to_num(batting_team_totals[7].text))
+	BB = int(convert_to_num(batting_team_totals[8].text))
+
+	pitching_team_totals = soup.find_all('div', 'ptstotal')[1].find_all('li')
+	IP  = float(convert_to_num(pitching_team_totals[0].text))
+	HRA = int(convert_to_num(pitching_team_totals[1].text))
+	BBA = int(convert_to_num(pitching_team_totals[2].text))
+	K   = int(convert_to_num(pitching_team_totals[3].text))
+
+	print DATE, team, AB, H, DB, TP, HR, SB, CS, BB, IP, HRA, BBA, K 
