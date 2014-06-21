5fb0a6ea18b21b5ff29e5a9aa1db550ffadbce01
Vinay Kumar <sdvinay@gmail.com>
2014-06-21 14:48:58 -0700

    Updated bs.py for 2014 HTML format

--- bs.py
+++ bs.py
@@ -6,16 +6,17 @@ import requests # http://docs.python-requests.org/en/latest/
 import datetime
 
 # constants/etc
-LEAGUE=58506 # weights two
+#LEAGUE=28858 # weights two
+LEAGUE=41702 # super lounge weights 
 URL = 'http://baseball.fantasysports.yahoo.com/b1/' + str(LEAGUE) + '/team'
 with open('cook2.txt') as f:
 	cookie_value = f.read()
-headers = {'Cookie' : cookie_value}
+headers = {'Cookie' : cookie_value, "Accept-Encoding": "gzip,deflate,sdch", "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
 
 # configuration
 STARTTEAM = int(sys.argv[1])
 NUMTEAMS = int(sys.argv[2])
-STARTDATE = datetime.date(2013,3,31)
+STARTDATE = datetime.datetime.strptime(sys.argv[3], "%Y-%m-%d")
 
 # funcs to convert the string formatted stats to numeric types
 def convert_to_int(stri):
@@ -26,8 +27,8 @@ def convert_to_float(stri):
 
 # a generator to yield the dates (since range() doesn't work on dates)
 def dates():
-	dt = STARTDATE
-	while dt < datetime.date.today():
+	dt = STARTDATE.date()
+	while dt <= datetime.date.today():
 		yield dt
 		dt += datetime.timedelta(1)
 
@@ -43,26 +44,28 @@ for dt in dates():
 			raise Exception("Error from Yahoo", r.status_code)
 		soup = bs4.BeautifulSoup(r.text)
 
-		# The two team stats totals (batting and pitching) are in:
-		#    <div class="sum ptstotal">
+		# The two team stats totals (batting and pitching) are in tfoots
 		# BeautifulSoup makes it easy to grab those into an array to pull from
-		totals_lines = soup.find_all('div', 'ptstotal')
+		footers = soup.find_all('tfoot')
 
-		batting_team_totals = totals_lines[0].find_all('li')
-		AB = convert_to_int(batting_team_totals[1].text)
-		H  = convert_to_int(batting_team_totals[2].text)
-		DB = convert_to_int(batting_team_totals[3].text)
-		TP = convert_to_int(batting_team_totals[4].text)
-		HR = convert_to_int(batting_team_totals[5].text)
-		SB = convert_to_int(batting_team_totals[6].text)
-		CS = convert_to_int(batting_team_totals[7].text)
-		BB = convert_to_int(batting_team_totals[8].text)
+		batting_team_totals = footers[0].find_all('td')
+		AB = convert_to_int(batting_team_totals[3].text)
+		H  = convert_to_int(batting_team_totals[4].text)
+		DB = convert_to_int(batting_team_totals[5].text)
+		TP = convert_to_int(batting_team_totals[6].text)
+		HR = convert_to_int(batting_team_totals[7].text)
+		SB = convert_to_int(batting_team_totals[8].text)
+		CS = convert_to_int(batting_team_totals[9].text)
+		BB = convert_to_int(batting_team_totals[10].text)
+		bat_pts = convert_to_float(batting_team_totals[11].text)
 
-		pitching_team_totals = totals_lines[1].find_all('li')
-		IP  = convert_to_float(pitching_team_totals[0].text)
-		HRA = convert_to_int(pitching_team_totals[1].text)
-		BBA = convert_to_int(pitching_team_totals[2].text)
-		K   = convert_to_int(pitching_team_totals[3].text)
+		pitching_team_totals = footers[1].find_all('td')
+		IP  = convert_to_float(pitching_team_totals[2].text)
+		HRA = convert_to_int(pitching_team_totals[3].text)
+		BBA = convert_to_int(pitching_team_totals[4].text)
+		K   = convert_to_int(pitching_team_totals[5].text)
+		pit_pts = convert_to_float(pitching_team_totals[6].text)
 
-		print datestr, team, AB, H, DB, TP, HR, SB, CS, BB, IP, HRA, BBA, K 
+		print datestr, team, AB, H, DB, TP, HR, SB, CS, BB, IP, HRA, BBA, K, bat_pts, pit_pts, bat_pts+pit_pts
+
 
